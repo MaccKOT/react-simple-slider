e034b57b23aab67bdb43b166d5f81ee3bd3801ba
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-11-09 13:54:34 +0400

    Slider css. Add arrows

--- src/carousel/Carousel.jsx
+++ src/carousel/Carousel.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState, Children, cloneElement } from 'react';
 import './Carousel.css';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 
 export const Carousel = ({ children }) => {
+  const [pages, setPages] = useState([]);
+  const PAGE_WIDTH = 450; //450px per item
+
+  const handleLeftArrowClick = () => {
+    console.log('left arrow click');
+  };
+  const handleRightArrowClick = () => {
+    console.log('left right click');
+  };
+
+  useEffect(() => {
+    setPages(
+      Children.map(children, (child) => {
+        return cloneElement(child, {
+          style: {
+            height: '100%',
+            minWidth: `${PAGE_WIDTH}px`,
+            maxWidth: `${PAGE_WIDTH}px`,
+          },
+        });
+      })
+    );
+  }, []);
+
   return (
     <div className='main-container'>
+      <FaChevronLeft className='arrow' onClick={handleLeftArrowClick} />
       <div className='window'>
-        <div className='all-pages-container'>{children}</div>
+        <div className='all-pages-container'>{pages}</div>
       </div>
+      <FaChevronRight className='arrow' onClick={handleRightArrowClick} />
     </div>
   );
 };
